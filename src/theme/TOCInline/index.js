3171876c8591ba10d3f4526add523a1f21c23f1f
凱恩Kane <50613gary@gmail.com>
2022-02-05 18:28:14 +0800

    new article

---
+++ src/theme/TOCInline/index.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from 'react';
+import styles from './styles.module.css';
+import TOCItems from '@theme/TOCItems';
+
+function TOCInline({toc, minHeadingLevel, maxHeadingLevel}) {
+	return (
+		<div className={styles.tableOfContentsInline}>
+			<span className={styles.tableOfContentsText}>本篇目錄</span>
+			<TOCItems
+				toc={toc}
+				minHeadingLevel={minHeadingLevel}
+				maxHeadingLevel={maxHeadingLevel}
+				className="table-of-contents"
+				linkClassName={null}
+			/>
+		</div>
+	);
+}
+
+export default TOCInline;
