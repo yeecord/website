6c6cfdb6b85316c3bf10395b51e0604b90e213e6
凱恩Kane <50613gary@gmail.com>
2022-02-28 22:02:28 +0800

    switch to typekit

--- src/theme/LayoutHead/index.js
+++ src/theme/LayoutHead/index.js
@@ -94,10 +94,10 @@ export default function LayoutHead(props) {
 				<title>{pageTitle}</title>
 				<meta property="og:title" content={pageTitle}/>
 				<meta name="twitter:card" content="summary_large_image"/>
-				<link href="https://fonts.gstatic.com/" rel="preconnect" crossOrigin="anonymous"/>
-				<link href="https://fonts.googleapis.com/" rel="preconnect" crossOrigin="anonymous"/>
 				
-				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&family=Readex+Pro&display=swap" crossOrigin="anonymous"/>
+				<script>
+					{`((d) => {var config = {kitId: 'lnl1ewv',scriptTimeout: 3000,async: true},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);`}
+				</script>
 			</Head>
 			
 			{/* image can override the default image */}
