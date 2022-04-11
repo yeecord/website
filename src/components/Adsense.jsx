10d8aa7b06320f3432ab4410808437e681af2c59
Gary50613 <50613gary@gmail.com>
2022-04-11 09:46:29 +0800

    remove multiplex

--- src/components/Adsense.jsx
+++ src/components/Adsense.jsx
@@ -2,12 +2,7 @@ import React, {useEffect} from 'react';
 import Head from "@docusaurus/Head";
 import styles from './Adsense.module.css';
 
-const units = {
-	default: ["7480799616", "auto"],
-	multiple: ["7480799616", "autorelaxed"]
-}
-
-export default function Adsense(props) {
+export default function Adsense() {
 	useEffect(() => {
 		try {
 			(window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -27,8 +22,8 @@ export default function Adsense(props) {
 						 aria-label="promote"
 						 style={{ display: 'block' }}
 						 data-ad-client="ca-pub-1801171681307308"
-						 data-ad-slot={units[props.unit || "default"][0]}
-						 data-ad-format={units[props.unit || "default"][1]}
+						 data-ad-slot="7480799616"
+						 data-ad-format="auto"
 						 data-full-width-responsive="true"/>
 				</div>
 		</div>
