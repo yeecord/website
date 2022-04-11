e77ad6a2e616e57f7dab524ebff7d2d343f996f3
Gary50613 <50613gary@gmail.com>
2022-04-11 09:21:32 +0800

    fix adsense

--- src/components/Adsense.jsx
+++ src/components/Adsense.jsx
@@ -2,7 +2,12 @@ import React, {useEffect} from 'react';
 import Head from "@docusaurus/Head";
 import styles from './Adsense.module.css';
 
-export default function Adsense() {
+const units = {
+	default: ["7480799616", "auto"],
+	multiple: ["7480799616", "autorelaxed"]
+}
+
+export default function Adsense(props) {
 	useEffect(() => {
 		try {
 			(window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -17,13 +22,15 @@ export default function Adsense() {
 					crossOrigin="anonymous"/>
 			</Head>
 				<span className={styles.text}>如果你對下面贊助商的內容有興趣可以點一下</span>
-				<ins className="adsbygoogle"
-					 aria-label="promote"
-					 style={{ display: 'block' }}
-					 data-ad-client="ca-pub-1801171681307308"
-					 data-ad-slot="7480799616"
-					 data-ad-format="auto"
-					 data-full-width-responsive="true"/>
+				<div style={{ minHeight: '280px' }}>
+					<ins className="adsbygoogle"
+						 aria-label="promote"
+						 style={{ display: 'block' }}
+						 data-ad-client="ca-pub-1801171681307308"
+						 data-ad-slot={units[props.unit || "default"][0]}
+						 data-ad-format={units[props.unit || "default"][1]}
+						 data-full-width-responsive="true"/>
+				</div>
 		</div>
 	)
 }
