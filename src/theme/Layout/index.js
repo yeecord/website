7b2bb76a9993a0d282275b4f647f485a1f76add7
凱恩Kane <50613gary@gmail.com>
2022-05-21 10:28:01 +0800

    finished shop

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -7,37 +7,47 @@ import Navbar from '@theme/Navbar';
 import Footer from '@theme/Footer';
 import LayoutProviders from '@theme/LayoutProviders';
 import {
-  PageMetadata,
-  ThemeClassNames,
-  useKeyboardNavigation,
+	PageMetadata,
+	ThemeClassNames,
+	useKeyboardNavigation,
 } from '@docusaurus/theme-common';
 import ErrorPageContent from '@theme/ErrorPageContent';
 import './styles.css';
+import Head from "@docusaurus/core/lib/client/exports/Head";
+
 export default function Layout(props) {
-  const {
-    children,
-    noFooter,
-    wrapperClassName,
-    // Not really layout-related, but kept for convenience/retro-compatibility
-    rawTitle,
-    description,
-  } = props;
-  useKeyboardNavigation();
-  return (
-    <LayoutProviders>
-      <PageMetadata title={rawTitle} description={description} />
-
-      <SkipToContent />
-
-      <AnnouncementBar />
-
-      <Navbar />
-
-      <div className={clsx(ThemeClassNames.wrapper.main, wrapperClassName)}>
-        <ErrorBoundary fallback={ErrorPageContent}>{children}</ErrorBoundary>
-      </div>
-
-      {!noFooter && <Footer />}
-    </LayoutProviders>
-  );
+	const {
+		children,
+		noFooter,
+		wrapperClassName,
+		// Not really layout-related, but kept for convenience/retro-compatibility
+		rawTitle,
+		description,
+	} = props;
+	useKeyboardNavigation();
+	return (
+		<LayoutProviders>
+			<PageMetadata title={rawTitle} description={description}/>
+			
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@500;700&display=fallback"
+                    rel="stylesheet"/>
+            </Head>
+            
+			<SkipToContent/>
+			
+			<AnnouncementBar/>
+			
+			<Navbar/>
+			
+			<div className={clsx(ThemeClassNames.wrapper.main, wrapperClassName)}>
+				<ErrorBoundary fallback={ErrorPageContent}>{children}</ErrorBoundary>
+			</div>
+			
+			{!noFooter && <Footer/>}
+		</LayoutProviders>
+	);
 }
