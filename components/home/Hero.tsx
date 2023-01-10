f3c701b35624b9de287c6e4a051156d14bf9f3b3
SonMooSans <god63820869@gmail.com>
2023-01-10 23:23:14 +0800

    Fix color issues

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -8,115 +8,128 @@ import styles from "./hero.module.css";
 import { noto } from "@components/home/HomePage";
 
 function Buttons() {
-  return (
-    <div className="grid grid-cols-1 w-full max-w-[400px] sm:w-fit sm:grid-cols-2 gap-3">
-      <LinkButton href="/docs" className="primary-button rounded-md w-full font-medium">
-        使用教學
-      </LinkButton>
-      <LinkButton
-        href="/invite"
-        target="_blank"
-        className="icon-button rounded-md w-full justify-center font-medium"
-      >
-        <IoMdOpen />
-        邀請機器人
-      </LinkButton>
-    </div>
-  );
+    return (
+        <div className="grid grid-cols-1 w-full max-w-[400px] sm:w-fit sm:grid-cols-2 gap-3">
+            <LinkButton
+                href="/docs"
+                className="primary-button rounded-md w-full font-medium"
+            >
+                使用教學
+            </LinkButton>
+            <LinkButton
+                href="/invite"
+                target="_blank"
+                className="icon-button rounded-md w-full justify-center font-medium"
+            >
+                <IoMdOpen />
+                邀請機器人
+            </LinkButton>
+        </div>
+    );
 }
 
 export function Hero() {
-  return (
-    <div
-      className={clsx(
-        "w-full flex flex-col gap-12 relative z-[2] mt-[6rem] md:mt-[10rem] xl:mt-[14rem] px-[1rem]",
-        "items-center text-center"
-      )}
-    >
-      <Gradient
-        src={HeroGradient}
-        className="absolute hidden -top-[200px] -right-0 lg:-right-[300px] min-w-[800px] w-full -z-[1]"
-      />
-      <h1
-        className={clsx(
-          "font-bold leading-[1.1]",
-          "text-6xl sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl font-sans",
-            noto.variable
-        )}
-      >
-        萬中選一的
-        <br className="md:hidden" />
-        <span
-          className={clsx(
-            "text-gradient bg-gradient-to-r from-blue-400 via-green-300 to-blue-400 mx-4",
-            styles["animated-gradient"]
-          )}
+    return (
+        <div
+            className={clsx(
+                "w-full flex flex-col gap-12 relative z-[2] mt-[6rem] md:mt-[10rem] xl:mt-[14rem] px-[1rem]",
+                "items-center text-center"
+            )}
         >
-          Discord
-        </span>
-        <br className="md:hidden" />
-        機器人
-      </h1>
-      <h2 className="heading-md md:text-3xl text-secondary-light font-medium dark:text-secondary-dark max-w-[450px] md:max-w-[700px]">
-        YEE式機器龍具有眾多功能且強大，讓你創造出優秀的中文 Discord 社群
-      </h2>
-      <Buttons />
-      <Servers />
-    </div>
-  );
+            <Gradient
+                src={HeroGradient}
+                className="absolute hidden -top-[200px] -right-0 lg:-right-[300px] min-w-[800px] w-full -z-[1]"
+            />
+            <h1
+                className={clsx(
+                    "font-bold leading-[1.1]",
+                    "text-6xl sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl font-sans",
+                    noto.variable
+                )}
+            >
+                萬中選一的
+                <br className="md:hidden" />
+                <span
+                    className={clsx(
+                        "text-gradient bg-gradient-to-r from-blue-400 via-green-300 to-blue-400 mx-4",
+                        styles["animated-gradient"]
+                    )}
+                >
+                    Discord
+                </span>
+                <br className="md:hidden" />
+                機器人
+            </h1>
+            <h2 className="heading-md md:text-3xl text-secondary font-medium max-w-[450px] md:max-w-[700px]">
+                YEE式機器龍具有眾多功能且強大，讓你創造出優秀的中文 Discord 社群
+            </h2>
+            <Buttons />
+            <Servers />
+        </div>
+    );
 }
 
 function Servers() {
-  return (
-    <div className="flex flex-col gap-5 mt-[3rem]">
-      <p className="text-lg font-semibold text-secondary mb-3">
-        各大伺服器一致好評
-      </p>
-      <div
-        className={clsx(
-          "flex flex-row justify-center gap-x-6 gap-y-3",
-          "overflow-hidden md:flex-wrap",
-          "mask-image-[linear-gradient(to_right,_transparent,_white,_transparent)] md:mask-image-[none]"
-        )}
-      >
-        <Server img="/home/customers/apex-tw.png" name="APEX Taiwan" />
-        <Server img="/home/customers/avery.png" name="Avery" transparent />
-        <Server img="/home/customers/chengfeng.png" name="承風飛躍" />
-        <div className="hidden sm:block basis-full" />
-        <Server
-          img="/home/customers/daidai.png"
-          name="老查呆呆の迷因調查局總部"
-        />
-        <Server img="/home/customers/empressival.png" name="Empressival" />
-        <Server img="/home/customers/myit.png" name="MyIT" />
-        <Server img="/home/customers/valorant-tw.png" name="VALORANT Taiwan" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-5 mt-[3rem]">
+            <p className="text-lg font-semibold text-secondary mb-3">
+                各大伺服器一致好評
+            </p>
+            <div
+                className={clsx(
+                    "flex flex-row justify-center gap-x-6 gap-y-3",
+                    "overflow-hidden md:flex-wrap",
+                    "mask-image-[linear-gradient(to_right,_transparent,_white,_transparent)] md:mask-image-[none]"
+                )}
+            >
+                <Server img="/home/customers/apex-tw.png" name="APEX Taiwan" />
+                <Server
+                    img="/home/customers/avery.png"
+                    name="Avery"
+                    transparent
+                />
+                <Server img="/home/customers/chengfeng.png" name="承風飛躍" />
+                <div className="hidden sm:block basis-full" />
+                <Server
+                    img="/home/customers/daidai.png"
+                    name="老查呆呆の迷因調查局總部"
+                />
+                <Server
+                    img="/home/customers/empressival.png"
+                    name="Empressival"
+                />
+                <Server img="/home/customers/myit.png" name="MyIT" />
+                <Server
+                    img="/home/customers/valorant-tw.png"
+                    name="VALORANT Taiwan"
+                />
+            </div>
+        </div>
+    );
 }
 
 function Server({
-  img,
-  name,
-  transparent,
+    img,
+    name,
+    transparent,
 }: {
-  img: string;
-  name: string;
-  transparent?: boolean;
+    img: string;
+    name: string;
+    transparent?: boolean;
 }) {
-  return (
-    <div className="h-stack gap-3 mx-1 text-gray-700 dark:text-gray-300 flex-shrink-0">
-      <Image
-        alt={name}
-        src={img}
-        width="50"
-        height="50"
-        className={clsx(
-          "rounded-full grayscale",
-          transparent && "brightness-[10%] dark:brightness-75"
-        )}
-      />
-      <h3 className="heading-md font-medium hidden sm:block">{name}</h3>
-    </div>
-  );
+    return (
+        <div className="h-stack gap-3 mx-1 text-gray-700 dark:text-gray-300 flex-shrink-0">
+            <Image
+                alt={name}
+                src={img}
+                width="50"
+                height="50"
+                className={clsx(
+                    "rounded-full grayscale",
+                    transparent && "brightness-[10%] dark:brightness-75"
+                )}
+            />
+            <h3 className="heading-md font-medium hidden sm:block">{name}</h3>
+        </div>
+    );
 }
--- components/home/RpgSystem.tsx
+++ components/home/RpgSystem.tsx
@@ -5,122 +5,122 @@ import Star from "@static/home/star.svg";
 import clsx from "clsx";
 
 export function RpgSystem() {
-  return (
-    <div
-      className={clsx(
-        "flex flex-col relative gap-5 items-start md:items-center",
-        "mt-[10rem] z-[2]"
-      )}
-    >
-      <Background />
+    return (
+        <div
+            className={clsx(
+                "flex flex-col relative gap-5 items-start md:items-center",
+                "mt-[10rem] z-[2]"
+            )}
+        >
+            <Background />
 
-      <h1 className="heading-xl">
-        <span className="text-gradient from-green-400 to-blue-400 dark:to-blue-200">
-          開創性
-        </span>
-        的角色扮演系統
-      </h1>
-      <h3 className="heading-md text-secondary">
-        進入這個奇幻世界，開始你的冒險吧！
-      </h3>
-      <LinkButton
-        href="/docs/rpg"
-        className="primary-button mt-4 bg-green-500 text-white dark:bg-green-300 dark:text-black"
-      >
-        開始使用
-      </LinkButton>
-      <Jobs />
-    </div>
-  );
+            <h1 className="heading-xl">
+                <span className="text-gradient from-green-400 to-blue-400 dark:to-blue-200">
+                    開創性
+                </span>
+                的角色扮演系統
+            </h1>
+            <h3 className="heading-md text-secondary">
+                進入這個奇幻世界，開始你的冒險吧！
+            </h3>
+            <LinkButton
+                href="/docs/rpg"
+                className="primary-button mt-4 bg-gradient-to-b to-green-600"
+            >
+                開始使用
+            </LinkButton>
+            <Jobs />
+        </div>
+    );
 }
 
 function Background() {
-  return (
-    <>
-      <Gradient
-        src={GreenGradient}
-        className="absolute -bottom-[100px] lg:-bottom-[40%] opacity-80 w-full min-w-[800px] -z-[1]"
-      />
-      <div
-        className={clsx(
-          "absolute top-0 left-0 -z-[1] opacity-80 dark:opacity-80 h-full",
-          "mask-image-[linear-gradient(to_bottom,_white_90%,_transparent)]"
-        )}
-      >
-        <Gradient
-          src={Star}
-          className={clsx(
-            "min-w-[900px] w-full object-cover",
-            "mask-image-[linear-gradient(to_top,_rgba(0,0,0,0.6),_transparent_85%)]"
-          )}
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Gradient
+                src={GreenGradient}
+                className="absolute -bottom-[100px] lg:-bottom-[40%] opacity-80 w-full min-w-[800px] -z-[1]"
+            />
+            <div
+                className={clsx(
+                    "absolute top-0 left-0 -z-[1] opacity-80 dark:opacity-80 h-full",
+                    "mask-image-[linear-gradient(to_bottom,_white_90%,_transparent)]"
+                )}
+            >
+                <Gradient
+                    src={Star}
+                    className={clsx(
+                        "min-w-[900px] w-full object-cover",
+                        "mask-image-[linear-gradient(to_top,_rgba(0,0,0,0.6),_transparent_85%)]"
+                    )}
+                />
+            </div>
+        </>
+    );
 }
 
 function Jobs() {
-  return (
-    <div className="grid text-start grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-3">
-      <Job
-        name="農夫"
-        description="和漁夫是差不多辛勤的職業，不過在這個世界，農夫的收益比漁夫還要高"
-      />
-      <Job
-        name="漁夫"
-        description="是個需要勞力的職業，你必須要努力勤奮的抓魚，才會獲得收益"
-      />
-      <Job
-        name="藥劑師"
-        description="是受一般人敬仰的職業，透過進行科學實驗生產藥水，能夠獲得很高的收益"
-      />
-      <Job
-        name="廚師"
-        description="跟農夫以及漁夫購買食材，再透過烘烤食物並轉賣來獲得收益"
-        optional
-      />
-      <Job
-        name="礦工"
-        description="在深不見底的洞窟裡挖礦，雖然可能沒有很好的收穫，不過有機會也可以挖到鑽石等好東西"
-      />
-      <Job
-        name="牧農"
-        description="飼養各類的禽類豬雞牛等還獲得肉類"
-        optional
-      />
-      <Job
-        name="鐵匠"
-        description="通常會和礦工合作來熔煉礦物並販賣出很高的價錢"
-        optional
-      />
-      <Job
-        name="伐木工"
-        description="在森林中砍伐木頭，是木頭的來源"
-        optional
-      />
-    </div>
-  );
+    return (
+        <div className="grid text-start grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-3">
+            <Job
+                name="農夫"
+                description="和漁夫是差不多辛勤的職業，不過在這個世界，農夫的收益比漁夫還要高"
+            />
+            <Job
+                name="漁夫"
+                description="是個需要勞力的職業，你必須要努力勤奮的抓魚，才會獲得收益"
+            />
+            <Job
+                name="藥劑師"
+                description="是受一般人敬仰的職業，透過進行科學實驗生產藥水，能夠獲得很高的收益"
+            />
+            <Job
+                name="廚師"
+                description="跟農夫以及漁夫購買食材，再透過烘烤食物並轉賣來獲得收益"
+                optional
+            />
+            <Job
+                name="礦工"
+                description="在深不見底的洞窟裡挖礦，雖然可能沒有很好的收穫，不過有機會也可以挖到鑽石等好東西"
+            />
+            <Job
+                name="牧農"
+                description="飼養各類的禽類豬雞牛等還獲得肉類"
+                optional
+            />
+            <Job
+                name="鐵匠"
+                description="通常會和礦工合作來熔煉礦物並販賣出很高的價錢"
+                optional
+            />
+            <Job
+                name="伐木工"
+                description="在森林中砍伐木頭，是木頭的來源"
+                optional
+            />
+        </div>
+    );
 }
 
 function Job({
-  name,
-  description,
-  optional,
+    name,
+    description,
+    optional,
 }: {
-  name: string;
-  description: string;
-  optional?: boolean;
+    name: string;
+    description: string;
+    optional?: boolean;
 }) {
-  return (
-    <div
-      className={clsx(
-        "card flex-col gap-3 dark:bg-[rgba(10,10,10,0.7)]",
-        optional ? "hidden" : "flex",
-        "md:flex"
-      )}
-    >
-      <h3 className="heading-md">{name}</h3>
-      <p className="text-secondary">{description}</p>
-    </div>
-  );
+    return (
+        <div
+            className={clsx(
+                "card flex-col gap-3 dark:bg-[rgba(10,10,10,0.7)]",
+                optional ? "hidden" : "flex",
+                "md:flex"
+            )}
+        >
+            <h3 className="heading-md">{name}</h3>
+            <p className="text-secondary">{description}</p>
+        </div>
+    );
 }
