c793846298a8f6c1b3adc70f039cf58ebe0ddda8
SonMooSans <god63820869@gmail.com>
2023-01-10 14:56:40 +0800

    Add Theme switch

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { BsCaretUpFill } from "react-icons/bs";
 import { IoMdOpen } from "react-icons/io";
-
+import { ThemeSwitch, Bleed } from "nextra-theme-docs";
 export type FooterCategory = {
     title: string;
     items: FooterItem[];
@@ -20,21 +20,7 @@ export function Footer({ categories }: { categories: FooterCategory[] }) {
     return (
         <div className="max-w-[90rem] mx-auto p-6 mt-6">
             <div className="flex flex-col sm:flex-row justify-between gap-4">
-                <div
-                    className={clsx(
-                        "flex flex-row gap-2 h-fit items-center mx-auto order-last mt-5",
-                        "sm:mx-0 sm:order-first sm:mt-0"
-                    )}
-                >
-                    <Image
-                        alt="logo"
-                        src="/logo_128x128.png"
-                        width={40}
-                        height={40}
-                        className="rounded-full"
-                    />
-                    <h1 className="font-bold text-xl">Yeecord</h1>
-                </div>
+                <Info />
                 {categories.map((category, i) => (
                     <Category key={i} category={category} />
                 ))}
@@ -42,6 +28,31 @@ export function Footer({ categories }: { categories: FooterCategory[] }) {
             <p className="text-secondary mt-10">
                 YEE式機器龍 © 2019 ~ {new Date(Date.now()).getFullYear()}
             </p>
+        </div>
+    );
+}
+
+function Info() {
+    return (
+        <div
+            className={clsx(
+                "flex flex-col gap-2 mx-auto order-last mt-5",
+                "sm:mx-0 sm:order-first sm:mt-0"
+            )}
+        >
+            <div className="flex flex-row gap-2 items-center">
+                <Image
+                    alt="logo"
+                    src="/logo_128x128.png"
+                    width={40}
+                    height={40}
+                    className="rounded-full"
+                />
+                <h1 className="font-bold text-xl">Yeecord</h1>
+            </div>
+            <div className="mx-auto sm:hidden">
+                <ThemeSwitch />
+            </div>
         </div>
     );
 }
