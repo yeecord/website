d97bee4bbd76ba55174758673a5e610bc4bcce5c
SonMooSans <god63820869@gmail.com>
2023-01-12 17:18:42 +0800

    Improve music section

--- components/home/features/Music.tsx
+++ components/home/features/Music.tsx
@@ -1,6 +1,11 @@
 import Image, { ImageProps } from "next/image";
 import { AiFillCaretRight } from "react-icons/ai";
-import { BsMusicNoteBeamed, BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import {
+    BsMusicNoteBeamed,
+    BsArrowLeft,
+    BsArrowRight,
+    BsArrowRightCircleFill,
+} from "react-icons/bs";
 import { Progress } from "../Progress";
 import KickBack from "@static/home/songs/kick-back.jpg";
 import FightSong from "@static/home/songs/eve-fight-song.jpg";
@@ -15,22 +20,34 @@ export function Music() {
     return (
         <div
             className={clsx(
-                "grid grid-cols-1 gap-4 relative z-[2] px-5 pt-5",
-                "lg:px-14 lg:grid-cols-[1fr_0.8fr] lg:gap-10",
+                "grid grid-cols-1 gap-4 relative z-[2] p-7 sm:max-lg:pr-10 pb-0",
+                "lg:grid-cols-[1fr_0.8fr]",
                 "bg-slate-100 dark:bg-slate-900 rounded-3xl overflow-hidden"
             )}
         >
             <Gradient
                 src={GreenGradient}
                 className={clsx(
-                    "absolute -bottom-[300px] -left-[30%] min-w-[800px] w-full opacity-60 -z-[1]",
+                    "absolute -bottom-[400px] -left-[30%] min-w-[800px] w-full opacity-60 -z-[1]",
                     "lg:-top-[50%] lg:opacity-80 lg:w-[1000px] lg:left-[40%]"
                 )}
             />
             <Content />
+            <div className="max-sm:hidden absolute top-0 right-8 lg:right-16 z-[2]">
+                <div className="h-36 w-[2px] bg-gradient-to-b from-green-400 to-cyan-600 mx-auto" />
+                <div
+                    className={clsx(
+                        "rounded-full p-4 bg-gradient-to-br from-green-400 to-cyan-600",
+                        "text-2xl text-white shadow-lg shadow-green-300/50",
+                        "lg:text-3xl"
+                    )}
+                >
+                    <BsMusicNoteBeamed />
+                </div>
+            </div>
             <div
                 className={clsx(
-                    "flex flex-col mt-2 w-[650px] -mr-[20rem]",
+                    "flex flex-col mt-2 w-[650px] -mr-[20rem] max-md:ml-5 max-lg:ml-8",
                     "max-h-[10rem] lg:max-h-full lg:mt-[5rem]"
                 )}
             >
@@ -65,23 +82,23 @@ export function Music() {
 
 function Content() {
     return (
-        <div className="flex flex-col gap-3 max-w-[750px] lg:max-w-none">
-            <h1 className={`${styles.heading} leading-[1.2]`}>
-                <span className="text-gradient text-4xl from-green-400 to-cyan-400">
-                    一個指令
-                </span>
-                <br /> 輕鬆播放音樂
-            </h1>
+        <div
+            className={clsx(
+                "flex flex-col gap-3 max-w-[750px] lg:max-w-none",
+                "sm:p-5 md:p-5"
+            )}
+        >
+            <h1 className={styles.heading}>輕鬆播放音樂</h1>
             <h3 className="heading-md text-secondary">
                 讓你可以和朋友一起收聽 Spotify 和 SoundCloud
                 上的音樂，並且可以自訂播放清單，讓分享好音樂變簡單
             </h3>
             <div className="h-stack mt-3">
                 <LinkButton
-                    className="primary-button bg-gradient-to-r from-cyan-500 to-green-500 font-medium"
+                    className="primary-button bg-gradient-to-r from-cyan-500 to-green-500 flex flex-row items-center gap-2"
                     href="/docs"
                 >
-                    了解更多
+                    了解更多 <BsArrowRightCircleFill className="inline h-fit" />
                 </LinkButton>
             </div>
         </div>
