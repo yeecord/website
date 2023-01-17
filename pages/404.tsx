504dbddd18a528f9de37359f639259c6ba830dfc
SonMooSans <god63820869@gmail.com>
2023-01-17 22:26:51 +0800

    Add not found page

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import { IoMdHeartEmpty } from "react-icons/io";
+
+export default function NotFound() {
+    return (
+        <div className="w-full h-[100vh] bg-white darK:bg-black flex flex-col items-center justify-center text-center">
+            <h2 className="text-4xl sm:text-5xl font-bold">
+                找不到頁面
+                <br />
+                <IoMdHeartEmpty className="inline" />
+                <span className="text-secondary text-xl">好像不在這裡...</span>
+            </h2>
+        </div>
+    );
+}
