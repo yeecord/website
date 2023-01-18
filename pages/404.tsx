0168db1e9c1e7169b77c61d70269c2f281f533ea
SonMooSans <god63820869@gmail.com>
2023-01-18 10:56:17 +0800

    Fix bug

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import { IoMdHeartEmpty } from "react-icons/io";
 
 export default function NotFound() {
     return (
-        <div className="w-full h-[100vh] bg-white darK:bg-black flex flex-col items-center justify-center text-center">
+        <div className="w-full h-[100vh] bg-white dark:bg-black flex flex-col items-center justify-center text-center">
             <h2 className="text-4xl sm:text-5xl font-bold">
                 找不到頁面
                 <br />
