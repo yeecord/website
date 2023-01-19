9253039cd5546cd29cbb502915e17f98c65e8bbe
SonMooSans <god63820869@gmail.com>
2023-01-19 14:00:21 +0800

    Improve community section

--- lib/home/Community.tsx
+++ lib/home/Community.tsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import { RiDiscordFill } from "react-icons/ri";
 import Gradient from "./components/Gradient";
 import LinkButton from "./components/LinkButton";
 import CyanPinkGradient from "@static/home/cyan-pink-gradient.svg";
@@ -8,48 +7,33 @@ import Image from "next/image";
 import clsx from "clsx";
 import { Tooltip, TooltipProvider, TooltipWrapper } from "react-tooltip";
 import formatter from "../../lib/utils/formatter";
+import { FaDiscord } from "react-icons/fa";
 
 const MotionImage = motion(Image);
 
 export function Community({ joined }: { joined: number }) {
     return (
-        <div className="flex flex-col w-full gap-5 items-center text-center mt-[5rem] py-[5rem] px-3 md:px-6 z-[2] relative">
+        <div className="flex flex-col w-full gap-5 items-center text-center mt-[2rem] py-[3rem] px-3 md:px-6 z-[2] relative">
             <Gradient
                 src={CyanPinkGradient}
                 className="absolute -z-[1] top-0 left-0 w-full overflow-visible object-cover h-full opacity-20"
             />
             <h1 className="heading-xl xl:text-6xl">
-                <span className="text-gradient from-cyan-400 to-pink-400 dark:from-pink-300 dark:to-cyan-300 mr-2">
+                <span className="text-cyan-400 [text-shadow:_#e66ad0_3px_5px] mr-2">
                     Discord
                 </span>
-                社群
+                <span className="text-pink-400">社群</span>
             </h1>
-            <h3 className="heading-md text-secondary text-medium">
-                參與我們的社群。我們歡迎大家！
+            <h3 className="heading-md  text-black dark:text-slate-200">
+                我們歡迎大家！
             </h3>
 
-            <JoinButton />
-            <div className="h-stack flex-wrap justify-center">
-                <h2
-                    className="font-bold text-6xl md:text-5xl bg-gradient-to-br text-gradient from-cyan-300 to-pink-300"
-                    style={{
-                        textShadow: "#22d3ee 2px 5px",
-                    }}
-                >
-                    {formatter.format(joined)}
-                </h2>
-                <h3
-                    className="text-2xl font-bold text-pink-400 dark:text-pink-300"
-                    style={{ textShadow: "#67e8f9 1px 1px" }}
-                >
-                    位成員已加入
-                </h3>
-            </div>
+            <JoinButton joined={joined} />
         </div>
     );
 }
 
-function JoinButton() {
+function JoinButton({ joined }: { joined: number }) {
     return (
         <div className="p-[6rem] z-[2] relative">
             <MotionImage
@@ -83,12 +67,13 @@ function JoinButton() {
                         href="https://discord.com/invite/yeecord"
                         target="_blank"
                         className={clsx(
-                            "icon-button p-3 w-fit h-fit rounded-full bg-gradient-to-br from-pink-500 to-purple-400",
-                            "text-5xl text-white shadow-xl shadow-pink-400/50"
+                            "icon-button p-3 w-fit h-fit rounded-full bg-gradient-to-br from-pink-400 to-cyan-400",
+                            "text-3xl text-white shadow-xl shadow-cyan-400/50"
                         )}
                         aria-label="Join"
                     >
-                        <RiDiscordFill />
+                        <FaDiscord className="text-5xl" />
+                        {formatter.format(joined)}
                     </LinkButton>
                 </TooltipWrapper>
             </TooltipProvider>
