39d0a83c4fcd7a93312acfb88b98b0f3f8822a4d
SonMooSans <god63820869@gmail.com>
2023-01-19 14:26:35 +0800

    Improve ads

--- lib/context/adsense.tsx
+++ lib/context/adsense.tsx
@@ -1,34 +1,40 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, ReactNode, useContext } from "react";
+import useSWRImmutable from "swr/immutable";
 
-export const adsenseContext = createContext({
-	state: "checking",
-	setState(_value: string) {}
-})
+export type AdsenseContext = {
+    state: "success" | "checking" | "failed";
+};
+export const adsenseContext = createContext<AdsenseContext>({
+    state: "checking",
+});
 
-export function AdsenseContextProvider({ children }: { children: any }) {
-	const [state, setState] = useState("checking")
+export function AdsenseContextProvider({ children }: { children: ReactNode }) {
+    const { isLoading, error } = useSWRImmutable(
+        ["check_adsense"],
+        () =>
+            fetch(
+                "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1801171681307308"
+            ),
+        {
+            shouldRetryOnError: false,
+        }
+    );
 
-	useEffect(() => {
-		(async () => {
-			try {
-				await fetch("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1801171681307308")
-			} catch(e) {
-				return setState("failed")
-			}
-
-			setState("ok")
-		})()
-	})
-
-	return (
-		<adsenseContext.Provider value={{
-			state, setState
-		}}>
-			{children}
-		</adsenseContext.Provider>
-	)
+    return (
+        <adsenseContext.Provider
+            value={{
+                state: isLoading
+                    ? "checking"
+                    : error != null
+                    ? "failed"
+                    : "success",
+            }}
+        >
+            {children}
+        </adsenseContext.Provider>
+    );
 }
 
 export function useAdsenseState() {
-	return useContext(adsenseContext)
+    return useContext(adsenseContext);
 }
