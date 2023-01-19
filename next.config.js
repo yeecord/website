2bc0040eb7acea8cf97b8ac2891a81290967745c
Gary50613 <50613gary@gmail.com>
2023-01-19 22:22:30 +0800

    rework for redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const {readFileSync} = require("fs")
+
+const redirectsFile = readFileSync("redirects.txt", "utf-8").toString().trim().split("\n")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
@@ -8,68 +12,15 @@ const nextConfig = {
         domains: ["cdn.discordapp.com"],
     },
     redirects() {
-        return [
-            {
-                source: "/invite",
-                destination: "https://app.yeecord.com/invite",
-                permanent: false,
-            },
-            {
-                source: "/docs/migrate-to-v4/",
-                destination: "/docs/",
-                permanent: true,
-            },
-            {
-                source: "/i",
-                destination: "https://app.yeecord.com/invite",
-                permanent: false
-            },
-            {
-                source: "/link",
-                destination: "https://app.yeecord.com/invite",
-                permanent: false
-            },
-            {
-                source: "/discord",
-                destination: "https://discord.gg/yeecord",
-                permanent: false
-            },
-            {
-                source: "/support",
-                destination: "https://discord.gg/yeecord",
-                permanent: false
-            },
-            {
-                source: "/tos",
-                destination: "/terms",
-                permanent: false
-            },
-            {
-                source: "/topgg",
-                destination: "https://top.gg/bot/584213384409382953",
-                permanent: false
-            },
-            {
-                source: "/dst",
-                destination: "https://discordservers.tw/bots/584213384409382953",
-                permanent: false
-            },
-            {
-                source: "/docs/intro",
-                destination: "/docs/",
-                permanent: true
-            },
-            {
-                source: "/rules",
-                destination: "https://hackmd.io/@yeecord/HJtapIa55",
-                permanent: false
-            },
-            {
-                source: "/github",
-                destination: "https://github.com/yeecord/docs",
-                permanent: false
+        return redirectsFile.map(args => {
+            const [source, destination, permanent] = args.split(" ")
+            
+            return {
+                source,
+                destination,
+                permanent: Boolean(permanent)
             }
-        ];
+        })
     },
 };
 
