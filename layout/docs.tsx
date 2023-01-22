3127338fcd313c7713697f5f4bdc8834b114aed0
SonMooSans <god63820869@gmail.com>
2023-01-22 16:45:55 +0800

    Use gradient instead of images

--- layout/docs.tsx
+++ layout/docs.tsx
@@ -1,7 +1,5 @@
 import clsx from "clsx";
 import { ReactNode } from "react";
-import GradientPng from "@static/docs/gradient.png";
-import Image from "next/image";
 import { PageOpts } from "nextra";
 import { DocsJsonLd } from "@utils/seo";
 import { DocsPageOpts } from "@utils/mdx";
@@ -18,9 +16,10 @@ export default function DocsLayout({
     return (
         <>
             <DocsJsonLd page={docs} />
-            <div className="absolute w-full top-0 left-0 -z-[1] overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden -z-[1]">
                 <Gradient />
             </div>
+
             {children}
         </>
     );
@@ -28,15 +27,14 @@ export default function DocsLayout({
 
 function Gradient() {
     return (
-        <Image
-            src={GradientPng}
-            alt=""
+        <div
             className={clsx(
-                "hidden dark:block",
-                "w-[70rem] h-[60rem] opacity-50 object-cover",
-                "-mt-[15rem] dark:-mt-[10rem] -ml-[5rem] sm:ml-0 max-w-none"
+                "absolute w-[50rem] h-[80rem] -top-[25rem] left-[5rem]",
+                "[mask-image:radial-gradient(farthest-corner,rgba(0,0,0,0.3)_0%,rgba(0,0,0,0.01)_65%,transparent)]",
+                "-rotate-[20deg]"
             )}
-            priority
-        />
+        >
+            <div className="bg-gradient-to-b from-cyan-400 via-blue-400 to-purple-800 w-full h-full" />
+        </div>
     );
 }
