1c7ab991444bb7b49957099668a7de00cd0a81a7
SonMooSans <god63820869@gmail.com>
2023-09-03 19:32:31 +0800

    Update Next Docs

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -7,7 +7,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/utils/cn";
 
 const itemVariants = cva(
-  "inline-flex flex-row items-center gap-2 rounded-md px-2 py-1.5 text-sm text-muted-foreground",
+  "inline-flex flex-row items-center gap-2 rounded-md px-2 py-1.5 text-sm text-muted-foreground hover:text-primary",
   {
     variants: {
       active: {
@@ -22,34 +22,36 @@ export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout
       tree={tree}
-      nav={false}
-      sidebarBanner={
-        <>
-          <Link
-            href="/"
-            className="mb-4 inline-flex flex-row gap-2 px-2 text-sm text-muted-foreground transition-colors hover:text-primary"
-          >
-            <Undo2Icon className="h-5 w-5" /> 返回主頁
-          </Link>
-          <div className="mb-4 flex flex-col lg:hidden">
+      nav={{ enabled: false }}
+      sidebar={{
+        banner: (
+          <>
             <Link
-              href="/docs"
-              className={cn(
-                itemVariants({
-                  active: true,
-                }),
-              )}
+              href="/"
+              className="mb-4 inline-flex flex-row gap-2 px-2 text-sm text-muted-foreground transition-colors hover:text-primary"
             >
-              <BookIcon className="h-5 w-5" />
-              使用教學
+              <Undo2Icon className="h-5 w-5" /> 返回主頁
             </Link>
-            <Link href="/blog" className={cn(itemVariants())}>
-              <LayoutListIcon className="h-5 w-5" />
-              部落格
-            </Link>
-          </div>
-        </>
-      }
+            <div className="mb-4 flex flex-col lg:hidden">
+              <Link
+                href="/docs"
+                className={cn(
+                  itemVariants({
+                    active: true,
+                  }),
+                )}
+              >
+                <BookIcon className="h-5 w-5" />
+                使用教學
+              </Link>
+              <Link href="/blog" className={cn(itemVariants())}>
+                <LayoutListIcon className="h-5 w-5" />
+                部落格
+              </Link>
+            </div>
+          </>
+        ),
+      }}
     >
       {children}
     </DocsLayout>
