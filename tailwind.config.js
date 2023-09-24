145e97d88f69ede362c1da4adc0c27ab1a066241
SonMooSans <god63820869@gmail.com>
2023-09-24 23:29:37 +0800

    Fix rounded

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,11 +45,6 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
-        sm: "calc(var(--radius) - 4px)",
-      },
       keyframes: {
         "accordion-down": {
           from: { height: 0 },
