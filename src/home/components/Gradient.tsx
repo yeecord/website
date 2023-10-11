31fff6991ccabe59f8c3dde00807692734a4f613
Gary50613 <50613gary@gmail.com>
2023-10-11 10:34:29 +0800

    fix gradient background alt

--- src/home/components/Gradient.tsx
+++ src/home/components/Gradient.tsx
@@ -4,7 +4,7 @@ import Image, { type ImageProps } from "next/image";
 export default function Gradient(props: Omit<ImageProps, "alt">) {
   return (
     <Image
-      alt=""
+      alt="gradient background"
       {...props}
       className={cn("pointer-events-none select-none", props.className)}
     />
