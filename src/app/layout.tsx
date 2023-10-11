453ab771a4f741fff733802ba393fdbe8c2dcac3
Gary50613 <50613gary@gmail.com>
2023-10-11 10:44:25 +0800

    missing alt for ring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const noto = Noto_Sans_TC({
 export const metadata: Metadata = {
   title: {
     template: "Yeecord - %s",
-    default: "Yeecord",
+    default: "Yeecord - 萬中選一的 Discord 機器人",
   },
   description:
     "YEE式機器龍的指令及使用教學，透過簡單的一鍵式指令以及中文介面的音樂功能快速建立好和朋友玩耍的優質空間",
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     images: "/opengraph-image.png",
     title: {
       template: "Yeecord - %s",
-      default: "Yeecord",
+      default: "Yeecord - 萬中選一的 Discord 機器人",
     },
     description:
       "YEE式機器龍的指令及使用教學，透過簡單的一鍵式指令以及中文介面的音樂功能快速建立好和朋友玩耍的優質空間",
