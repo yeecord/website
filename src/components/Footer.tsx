b4e603d8e2fcfce452a9fdf1a5997554f79314ae
Gary50613 <50613gary@gmail.com>
2023-10-19 16:24:33 +0800

    update icon import

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+
 import { ExternalLinkIcon } from "lucide-react";
 
 export type FooterCategory = {
---
+++ src/lucide-react.d.ts
@@ -0,0 +1,3 @@
+declare module "lucide-react/dist/esm/icons/*" {
+  export { LucideIcon as default } from "lucide-react";
+}
