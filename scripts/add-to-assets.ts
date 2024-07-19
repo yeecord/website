aaf1527a5a0f0fcd5e95812be6ea8d5c9ace5d14
fuma-nama <god63820869@gmail.com>
2024-07-19 19:56:10 +0800

    Upgrade fumadocs

--- scripts/add-to-assets.ts
+++ scripts/add-to-assets.ts
@@ -1,7 +1,7 @@
 import { createOgImage } from "./og";
 import { mkdir, readFile, writeFile } from "node:fs/promises";
 import { Resvg } from "@resvg/resvg-js";
-import { join, dirname } from "node:path";
+import { join, dirname, resolve } from "node:path";
 import type { SearchIndex } from "fumadocs-mdx";
 
 const path = ".next/server/chunks/fumadocs_search.json";
@@ -14,10 +14,14 @@ async function main() {
   // Copy fumadocs_search.json to `out` folder
   await writeFile("out/_map_indexes.json", JSON.stringify(indexes));
 
+  const noto = await readFile(
+    resolve(process.cwd(), "./public/noto-sans-semi-bold.woff"),
+  );
+
   // Open Graph Images
   await Promise.all(
     indexes.map(async (index) => {
-      const svg = await createOgImage(index);
+      const svg = await createOgImage(index, noto);
 
       const image = new Resvg(svg);
       const out = join("./out/og", `${index.url}.png`);
