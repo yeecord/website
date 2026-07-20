74549ef475bb0f02ff63ad0d39a5eec1dfeb2e63
Kane Wang <kane@yeecord.com>
2026-07-21 04:05:13 +0800

    og: ellipsis overflow, simplify branding

--- src/og-image.tsx
+++ src/og-image.tsx
@@ -5,7 +5,7 @@ function Logo({ size }: { size: number }) {
     <svg width={size} height={size} viewBox="0 0 128 128">
       <path
         d="M43.3522 25.2578C52.946 9.29013 72.5335 4.51549 81.128 4.12413C85.15 3.46785 94.7859 5.5398 99 7.38145C103.214 9.22311 107.699 10.3815 114.5 18.8815C121.301 27.3815 123 41.8815 123 51.8815C123 61.8815 118.222 77.4108 109.31 84.5494C107.413 86.0689 103.482 87.3457 98.5168 88.4495V115.881C98.5168 120.3 94.9351 123.881 90.5168 123.881H57.3483C52.9301 123.881 49.3483 120.3 49.3483 115.881V94.9799C35.5571 94.9799 23.5649 92.768 17.5687 89.8328C11.5725 86.8976 1.97863 78.6789 4.37712 65.1769C6.85518 51.2269 23.5648 46.9785 33.1587 48.7396C33.7583 46.3914 33.7583 41.2254 43.3522 25.2578Z"
-        fill="#111411"
+        fill="#4ade80"
       />
     </svg>
   );
@@ -52,6 +52,7 @@ export function OgImage({
             lineHeight: 1.25,
             margin: 0,
             lineClamp: 2,
+            textOverflow: "ellipsis",
           }}
         >
           {title}
@@ -65,6 +66,7 @@ export function OgImage({
               margin: 0,
               marginTop: "28px",
               lineClamp: 2,
+              textOverflow: "ellipsis",
             }}
           >
             {description}
@@ -76,38 +78,15 @@ export function OgImage({
             display: "flex",
             flexDirection: "row",
             alignItems: "center",
-            justifyContent: "space-between",
+            gap: "20px",
             marginTop: "auto",
           }}
         >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              gap: "16px",
-              backgroundColor: GREEN,
-              borderRadius: "16px",
-              padding: "12px 28px 12px 16px",
-            }}
-          >
-            <Logo size={44} />
-            <p
-              style={{
-                fontSize: "34px",
-                fontWeight: 800,
-                color: "#111411",
-                margin: 0,
-              }}
-            >
-              Yeecord
-            </p>
-          </div>
+          <Logo size={52} />
           <p
             style={{
-              fontSize: "28px",
-              fontWeight: 600,
-              color: "#a7b0aa",
+              fontSize: "36px",
+              fontWeight: 800,
               margin: 0,
             }}
           >
